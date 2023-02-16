e6605e140ad0f218b01f41efb45a91b6a8eb40bd
Ahmedl0oki <ahmed>
2023-02-16 03:42:00 -0800

    OK

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#inclode <stdio.h>
+#include <stdio.h>
 /* 
  * main - Entry point
  * Description: prints "Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
