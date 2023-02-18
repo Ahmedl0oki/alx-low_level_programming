6433919f95a6e3dd77fbd56df1ea2a139d7f9eca
Ahmedl0oki <ahmed>
2023-02-18 04:03:27 -0800

    OK

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * 
+ */
+int main(void)
+{
+	print("and that piece of art is useful\" \- Dora Korpar, 2015-10-19\n");
+	return(1);
+}
